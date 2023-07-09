7c753ff302cb1c8ca4d08a5bdec2c7b8b916982b
Adam R <riaz_adam@hotmail.com>
2023-07-09 04:33:22 +0100

    Add adaptive image card example

---
+++ demo/js/AdaptiveImageCard.js
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { Webhook } = require("../../src");
+const { demoUrl } = require("../constants");
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+async function send() {
+    try {
+        const data = {
+            "type": "message",
+            "attachments": [
+                {
+                    "contentType": "application/vnd.microsoft.card.hero",
+                    "content": {
+                        "title": "Seattle Center Monorail",
+                        "subtitle": "Seattle Center Monorail",
+                        "text": "The Seattle Center Monorail is an elevated train line between Seattle Center (near the Space Needle) and downtown Seattle. It was built for the 1962 World's Fair. Its original two trains, completed in 1961, are still in service.",
+                        "images": [
+                            {
+                                "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Seattle_monorail01_2008-02-25.jpg/1024px-Seattle_monorail01_2008-02-25.jpg"
+                            }
+                        ],
+                        "buttons": [
+                            {
+                                "type": "openUrl",
+                                "title": "Official website",
+                                "value": "https://www.seattlemonorail.com"
+                            },
+                            {
+                                "type": "openUrl",
+                                "title": "Wikipeda page",
+                                "value": "https://en.wikipedia.org/wiki/Seattle_Center_Monorail"
+                            }
+                        ]
+                    }
+                }
+            ],
+
+        }
+        const webook = new Webhook(demoUrl, data);
+        const response = await webook.sendMessage();
+        console.log(response);
+    } catch (error) {
+        console.log(error)
+    }
+}
